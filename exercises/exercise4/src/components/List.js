921bb682f7a087911b36c7edb31d1e0cf24fe7f7
Christopher Hasbani <chasbani@umd.edu>
2022-01-20 23:45:28 -0500

    Exercise4

--- exercises/exercise4/src/components/List.js
+++ exercises/exercise4/src/components/List.js
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
 
 class List extends Component {
-  
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+        <div>
+            <ul id="lis">
+                {this.props.data}
+            </ul>
+        </div>
+        )
+    }
+    
 }
+export default List;
--- exercises/exercise4/src/components/Search.js
+++ exercises/exercise4/src/components/Search.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import List from "./List";
 
 class Search extends Component {
   
+  constructor(props) {
+    super(props);
+    this.update = this.update.bind(this);
+    this.state = {
+      places: null,
+      check: []
+    };
+  }
+  render() {
+      return (
+        <div>
+          <input type="text" id="in" onChange={() => this.update()}/>
+          <List data={this.state.check}/>
+        </div>
+      );
+  }
+  update() {
+    let temp = document.getElementById("in").value;
+    if (temp == "") {
+      temp = [];
+    } else {
+      temp = this.state.places.filter(e => e.state.includes(temp) || e.city.includes(temp)).map(e => <li key={e.latitude}>{`${e.city}, ${e.state}`}</li>);
+    }
+    this.setState({check: temp});  
 
+  }
   /* DO NOT MODIFY */
   componentDidMount() {
     const allPlaces = [];
@@ -17,4 +43,6 @@ class Search extends Component {
       .catch(error => console.log(error));
   }
 }
+
+export default Search;
 
